8be4e47413ea05d948d678d9f0b8a48cb3638fc9
sirwilliamiv <wjconnolly2@gmail.com>
2017-05-08 13:33:04 -0500

    added createUser  in User controller

---
+++ controllers/userCtrl.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const User = require('../models/User');
+const { knex } = require('../db/database');
+
+
+//add user
+module.exports.createUser = ({ body: {name, email, password}, res, err) => {
+  User.findOneByEmail(email)
+  .then( (user) => {
+    if (user) {
+      res.status(409).json({msg: `User with ${email} already exists`})
+    } else {
+      return User.forge({name, email, password})
+      .save()
+      .then((data) => {
+        res.status(201).json(msg: 'user added successfully!')
+        console.log("user added", data)
+      })
+      .catch((err) => {
+        console.log("user not added successfully userCtrl,createUser function", err)
+        return err
+      })
+    }
+  })
+  .catch((err) => {
+    console.log("err findingbyemail", err)
+    return err
+  })
+}
+
+ // comparePassword: (passwordString) => {
+ //    return compare(passwordStr, this.attributes.password)
+ //  }, {
+ //    findByEmail: (email) => {
