9173a06f2e89df7a6432899aa8eea8c32bd50965
sirwilliamiv <wjconnolly2@gmail.com>
2017-05-08 11:38:42 -0500

    resolved passport-local unmet dependency locally, created song model

--- knexfile.js
+++ knexfile.js
@@ -14,22 +14,6 @@ module.exports = {
     seeds: {
       directory: __dirname + '/db/seeds'
     }
-  };
+  }
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
-// };
+};
