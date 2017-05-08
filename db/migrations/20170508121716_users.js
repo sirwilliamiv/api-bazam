d2a4e529d1fd8e1b7f13c0f6cbb39695d493a572
sirwilliamiv <wjconnolly2@gmail.com>
2017-05-08 12:42:29 -0500

    created tables in postgressql and added migrations/wrote schema

---
+++ db/migrations/20170508121716_users.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema
+  .createTable('users', function(table) {
+    table.increments();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.string('password').notNullable();
+  })
+
+
+
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+
+};
---
+++ db/migrations/20170508123207_songs.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise) {
+
+  return knex.schema
+  .createTable('songs', function(table) {
+    table.increments();
+    table.string('songartist').notNullable();
+    table.string('songname').notNullable();
+    table.string('songalbum').notNullable();
+    table.integer('userid').unsigned().index().references('id').inTable('users')
+    //
+  })
+
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('songs');
+
+};
