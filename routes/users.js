552a4ccd747537c646fa90230e0b7f0451c74de8
sirwilliamiv <wjconnolly2@gmail.com>
2017-05-15 15:30:23 -0500

    took out login post route

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const { login,logout } =require('../controllers/sessionCtrl');
 
 router.post('/user/new', createUser)
 // router.post('/user/login', login)
-router.post('/user/logout', logout)
+// router.post('/user/logout', logout)
 
 
 
