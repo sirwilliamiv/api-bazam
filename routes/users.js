faf943c5c83a2496738fb09f8bf1eaa5393ce26e
sirwilliamiv <wjconnolly2@gmail.com>
2017-05-15 15:32:16 -0500

    moving lines of code so heroku recognizes change

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,11 @@ const { login,logout } =require('../controllers/sessionCtrl');
 
 
 router.post('/user/new', createUser)
+
+
+
+
+
 // router.post('/user/login', login)
 // router.post('/user/logout', logout)
 
